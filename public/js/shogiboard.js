759e6a3a59aa41c39d9035475dab62e55c6fa061
myokoym <myokoym@gmail.com>
2010-08-31 23:50:31 +0900

    ver.0.1.0 release. + a faw patch.

--- public/js/shogiboard.js
+++ public/js/shogiboard.js
@@ -15,6 +15,38 @@ $(document).ready(function(){
       $("#flag").text("false");
     }
   });
+  $("#sakasa").click(function(){
+    if($("#flag").text()=="true"){
+      $target = $("#"+$("#coordinate").text());
+      if($target.html().match("<img.*>")){
+        $src = $target.find("img").attr("src");
+        if($target.html().match("_invert")){
+          $src = $src.replace("_invert", "");
+        }else{
+          $src = $src.replace(".gif", "_invert.gif");
+        }
+        $target.find("img").attr("src", $src);
+        $("#before").text($target.html());
+      }
+    }
+  });
+  $("#naru").click(function(){
+    if($("#flag").text()=="true"){
+      $target = $("#"+$("#coordinate").text());
+      if($target.html().match("<img.*>")){
+        if($target.html().match("fu|kyo|kei|gin|kaku|hi")){
+          $src = $target.find("img").attr("src");
+          if($target.html().match("nari_")){
+            $src = $src.replace("nari_", "");
+          }else{
+            $src = $src.replace("img/", "img/nari_");
+          }
+          $target.find("img").attr("src", $src);
+          $("#before").text($target.html());
+        }
+      }
+    }
+  });
   $("#bottom_marker").addClass("hidden");
 });
 
