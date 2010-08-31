316cd14bf1e0bf670c4957276acfe2aabf30c34f
myokoym <myokoym@gmail.com>
2010-08-31 21:59:01 +0900

    bad plan for save that get koma auto stock

--- public/js/shogiboard.js
+++ public/js/shogiboard.js
@@ -1,21 +1,29 @@
 $(function(){
-  $("td").click(function(){
+  $("td.cell").click(function(){
     if($("#flag").text()=="false"){
       $("#coordinate").html($(this).attr("id"));
-      //if($(this).html().match("<img.*>")){
-      if(true){
+      if($(this).html().match("<img.*>")){
         $("#before").text($(this).html());
         $("#flag").text("true");
         $(this).css("background-color", "red");
       }
     }else{
-      $before = $("#"+$("#coordinate").text())
-      $before.html($(this).html())
+      $before = $("#"+$("#coordinate").text());
+      if($(this).attr("id")!=$("#coordinate").text()){
+        if($(this).html().match("<img.*>")){
+          if($("img",this).attr("class")=="me"){
+            $("tr#top").append("<td id=\"cell\">"+$(this).html()+"</td>");
+          }else{
+            $("tr#bottom").append("<td id=\"cell\">"+$(this).html()+"</td>");
+          }
+        }
+        $before.html("　");
+        $(this).html($("#before").text());
+      }
       $before.css("background-color", "");
-      $(this).html($("#before").text());
       $("#flag").text("false");
     }
   });
-  $("#bottom_marker").css("display","none");
+  $("#bottom_marker").addClass("hidden");
 });
 
