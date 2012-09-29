d6c119dd9d4390cd1e47dca7a93583f78999deb0
Masafumi Yokoyama <myokoym@gmail.com>
2012-09-29 18:49:43 +0900

    Fix shogiboard.js generate by shogiboard.coffee

--- public/js/shogiboard.js
+++ public/js/shogiboard.js
@@ -1,52 +1,58 @@
-$(document).ready(function(){
-  $("td.cell").click(function(){
-    if($("#flag").text()=="false"){
-      $("#coordinate").html($(this).attr("id"));
-      if($(this).html().match("img")){
-        $("#before").text($(this).html());
-        $("#flag").text("true");
-        $(this).css("background-color", "red");
-      }
-    }else{
-      $before = $("#"+$("#coordinate").text());
-      $before.html($(this).html());
-      $before.css("background-color", "");
-      $(this).html($("#before").text());
-      $("#flag").text("false");
-    }
-  });
-  $("#sakasa").click(function(){
-    if($("#flag").text()=="true"){
-      $target = $("#"+$("#coordinate").text());
-      if($target.html().match("img")){
-        $src = $target.find("img").attr("src");
-        if($target.html().match("_invert")){
-          $src = $src.replace("_invert", "");
-        }else{
-          $src = $src.replace(".gif", "_invert.gif");
+(function() {
+
+  $(document).ready(function() {
+    $("td.cell").click(function() {
+      var $before;
+      if ($("#flag").text() === "false") {
+        $("#coordinate").html($(this).attr("id"));
+        if ($(this).html().match("img")) {
+          $("#before").text($(this).html());
+          $("#flag").text("true");
+          return $(this).css("background-color", "red");
         }
-        $target.find("img").attr("src", $src);
-        $("#before").text($target.html());
+      } else {
+        $before = $("#" + $("#coordinate").text());
+        $before.html($(this).html());
+        $before.css("background-color", "");
+        $(this).html($("#before").text());
+        return $("#flag").text("false");
       }
-    }
-  });
-  $("#naru").click(function(){
-    if($("#flag").text()=="true"){
-      $target = $("#"+$("#coordinate").text());
-      if($target.html().match("img")){
-        if($target.html().match("fu|kyo|kei|gin|kaku|hi")){
+    });
+    $("#sakasa").click(function() {
+      var $src, $target;
+      if ($("#flag").text() === "true") {
+        $target = $("#" + $("#coordinate").text());
+        if ($target.html().match("img")) {
           $src = $target.find("img").attr("src");
-          if($target.html().match("nari_")){
-            $src = $src.replace("nari_", "");
-          }else{
-            $src = $src.replace("img/", "img/nari_");
+          if ($target.html().match("_invert")) {
+            $src = $src.replace("_invert", "");
+          } else {
+            $src = $src.replace(".gif", "_invert.gif");
           }
           $target.find("img").attr("src", $src);
-          $("#before").text($target.html());
+          return $("#before").text($target.html());
         }
       }
-    }
+    });
+    $("#naru").click(function() {
+      var $src, $target;
+      if ($("#flag").text() === "true") {
+        $target = $("#" + $("#coordinate").text());
+        if ($target.html().match("img")) {
+          if ($target.html().match("fu|kyo|kei|gin|kaku|hi")) {
+            $src = $target.find("img").attr("src");
+            if ($target.html().match("nari_")) {
+              $src = $src.replace("nari_", "");
+            } else {
+              $src = $src.replace("img/", "img/nari_");
+            }
+            $target.find("img").attr("src", $src);
+            return $("#before").text($target.html());
+          }
+        }
+      }
+    });
+    return $("#bottom_marker").addClass("hidden");
   });
-  $("#bottom_marker").addClass("hidden");
-});
 
+}).call(this);
